70e946944d23abea1e98a592ec5b3c5fed047c8a
Mani <mani30saravanan@gmail.com>
2025-10-09 11:48:34 +0530

    Day six completed

---
+++ corejava_practce/src/com/tnsif/daysix/usingfinal/FinalClassDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.daysix.usingfinal;
+
+final class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+}
+class FinalChildClass extends FinalClass {
+}
+
+public class FinalClassDemo {
+	public static void main(String[] args) {
+		FinalClass f1 = new FinalClass(); 
+		f1.show();
+	}
+}
